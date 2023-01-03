60d55aeb32e32351dc6b0d92a754354faefbe5ea
A,Mao <88200382+AMao-0512@users.noreply.github.com>
2023-01-03 15:21:14 +0800

    Update mass_data_align.py

--- mass/mass_data_align.py
+++ mass/mass_data_align.py
@@ -13,9 +13,11 @@ samples' id.
 second col's should be m/z data.
 4.For standard dataset sheet, the data of the first col should be compounds informations(like a ID), 
 the second cols' data should be compounds' molecular mass(The ionized weight is calculated in the program, 
-adding only the weight of one sodium ion or one hydrogen ion.)
+adding only the weight of one sodium ion or one hydrogen ion). And one more thing about dataset sheet, 
+which is important, is that the data in the table should be arranged in ascending or descending order 
+according to the m/z ratio.
 5.For control group sheet, there only need one col, which contain the m/z ratio of the control set.
-6.For all sheets, data in the first row will be discarded(I'd like to keep titles~)
+6.For all sheets, data in the first row will be discarded.
 Written by: Deqiang AMao :)
 '''
 import xlwt
